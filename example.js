b0117fcd2911954e8d1d19c952048d2bee772c7a
whirlibulf <whirlibulf.engine@gmail.com>
2013-05-07 21:05:56 +0100

    Make components unique per object
    Index objects by ID string, not array index

--- example.js
+++ example.js
@@ -17,12 +17,12 @@ var scripts = new ScriptSystem();
 
 game.debug.on();
 
-game.systems.register(canvas);
-game.systems.register(scripts);
+game.addSystem(canvas);
+game.addSystem(scripts);
 
-game.components.register(require('transform'));
-game.components.register(require('primitive-shape'));
-game.components.register(require('script'));
+game.addComponent(require('transform'));
+game.addComponent(require('primitive-shape'));
+game.addComponent(require('script'));
 
 //game.loop.start(30);
 game.start(30);
@@ -31,34 +31,25 @@ game.start(30);
 
 //low level:
 var obj = game.manager.createObject('object id');
-game.manager.addComponentToObject(obj, 'Transform', 'transform', {'x': 0, 'y': 0});
-game.manager.addComponentToObject(obj, 'Sprite', 'primitive-shape', {'shape': 'circle', 'radius': 5});
-game.manager.addComponentToObject(obj, 'ClickHandler', 'script', scriptRef);
+game.addComponentToObject(obj, 'transform', {'x': 0, 'y': 0});
+game.addComponentToObject(obj, 'primitive-shape', {'shape': 'circle', 'radius': 5});
+game.addComponentToObject(obj, 'script', scriptRef);
 
 
 
 //data-driven:
 var prefab = {
   'Transform': {
-    'component': 'transform',
-    'options': {
-      'x': 0,
-      'y': 0
-    }
+    'x': 0,
+    'y': 0
   },
   'Sprite': {
-    'component': 'primitive-shape',
-    'options': {
-      'shape': 'circle',
-      'radius': 5,
-      'layer': 0
-    }
+    'shape': 'circle',
+    'radius': 5,
+    'layer': 0
   },
   'ClickHandler': {
-    'component': 'script',
-    'options': {
-      'script': scriptRef
-    }
+    'script': scriptRef
   }
 };
 
