0aa5dd5e356c3e67d5d1c11304a04735df8a832f
whirlibulf <whirlibulf.engine@gmail.com>
2013-05-18 20:19:12 +0100

    Simplify the API (breaking changes)

--- test/spec/game.js
+++ test/spec/game.js
@@ -20,34 +20,35 @@ describe("Game", function () {
   });
 
   it("should initialise all the core functions of whirlibulf", function () {
-    expect(game.addSystem).toBeDefined();
-    expect(game.addComponent).toBeDefined();
-    expect(game.createObject).toBeDefined();
+    expect(game.use).toBeDefined();
+    expect(game.create).toBeDefined();
+    expect(game.add).toBeDefined();
+    expect(game.get).toBeDefined();
+    expect(game.remove).toBeDefined();
     expect(game.loop).toBeDefined();
+    expect(game.start).toBeDefined();
   });
 
-  describe("addSystem", function () {
+  describe("use", function () {
     it("should add a system object to the game", function () {
-      game.addSystem({});
+      game.use({});
 
       expect(game._systems.length).toEqual(1);
 
-      game.addSystem({});
+      game.use({});
 
       expect(game._systems.length).toEqual(2);
     });
 
     it("should send a 'register' event to the system", function () {
-      game.addSystem(mock.system);
+      game.use(mock.system);
 
       expect(mock.system.init).toHaveBeenCalled();
     });
-  });
 
-  describe("addComponent", function () {
     it("should add a component factory to the game", function () {
       var result;
-      result = game.addComponent('test', function () {});
+      result = game.use('test', function () {});
 
       expect(game._components.test).toBeDefined();
       expect(result).toBe(true);
@@ -55,12 +56,12 @@ describe("Game", function () {
 
     it("should not add duplicate component types", function () {
       var result;
-      result = game.addComponent('test', function () {});
+      result = game.use('test', function () {});
 
       expect(game._components.test).toBeDefined();
       expect(result).toBe(true);
 
-      result = game.addComponent('test', function () {});
+      result = game.use('test', function () {});
 
       expect(game._components.test).toBeDefined();
       expect(result).toBe(false);
@@ -69,31 +70,31 @@ describe("Game", function () {
     it("should reject components without an ID", function () {
       var result;
 
-      result = game.addComponent(function () {});
+      result = game.use(function () {});
       expect(result).toBe(false);
 
-      result = game.addComponent();
+      result = game.use();
       expect(result).toBe(false);
 
-      result = game.addComponent(undefined, function () {});
+      result = game.use(undefined, function () {});
       expect(result).toBe(false);
 
-      result = game.addComponent('', function () {});
+      result = game.use('', function () {});
       expect(result).toBe(false);
     });
   });
 
-  describe("createObject", function () {
+  describe("create", function () {
     it("should reject objects without an ID", function () {
       var result;
-      result = game.createObject();
+      result = game.create();
 
       expect(result).toBe(false);
     });
 
     it("should create an empty object if no template is supplied", function () {
       var result;
-      result = game.createObject('test');
+      result = game.create('test');
 
       expect(result).toBe(true);
       expect(game._objects.test).toBeDefined();
@@ -101,86 +102,92 @@ describe("Game", function () {
 
     it("should not create duplicate objects with the same ID", function () {
       var result;
-      result = game.createObject('test');
+      result = game.create('test');
 
       expect(result).toBe(true);
       expect(game._objects.test).toBeDefined();
 
-      result = game.createObject('test');
+      result = game.create('test');
 
       expect(result).toBe(false);
       expect(game._objects.test).toBeDefined();
     });
   });
 
-  describe("getObject", function () {
+  describe("get", function () {
     it("should return the correct object", function () {
       var obj;
-      game.createObject('test1');
-      game.createObject('test2');
+      game.create('test1');
+      game.create('test2');
 
-      obj = game.getObject('test1');
+      obj = game.get('test1');
       expect(obj.id).toBe('test1');
 
-      obj = game.getObject('test2');
+      obj = game.get('test2');
       expect(obj.id).toBe('test2');
     });
 
     it("should return undefined if the object does not exist", function () {
-      expect(game.getObject('test')).not.toBeDefined();
+      expect(game.get('test')).not.toBeDefined();
     });
   });
 
-  describe("removeObject", function () {
+  describe("remove", function () {
     it("should be tested", function () {
       expect(false).toBe(true);
     });
   });
 
-  describe("createComponent", function () {
+  describe("add", function () {
+    it("should not do anything if the object does not exist", function () {
+      var result;
+      result = game.add('test', 'test', {});
+
+      expect(result).toBe(false);
+    });
+
     it("should not do anything if the component does not exist", function () {
       var result;
-      result = game.createComponent('test');
+      game.create('test');
+      result = game.add('test', 'test', {});
+
+      expect(result).toBe(false);
+    });
+
+    it("should add a new component instance to the object", function () {
+      game.use('test', mock.component);
+      game.create('test');
+
+      result = game.add('test', 'test', {});
+
+      expect(result).toBe(true);
+
+      //make sure object exists
+      result = game.get('test');
+      expect(result).toBeDefined();
+      expect(result.test).toBeDefined();
+
+      //make sure component exists
+      result = game.get('test', 'test');
+      expect(result).toBeDefined();
+    });
+  });
+
+  describe("_createComponent", function () {
+    it("should not do anything if the component does not exist", function () {
+      var result;
+      result = game._createComponent('test');
 
       expect(result).toBe(false);
     });
 
     it("should return the component instance's index after it is created", function () {
       var result;
-      game.addComponent('test', mock.component);
-      result = game.createComponent('test');
+      game.use('test', mock.component);
+      result = game._createComponent('test');
 
       expect(result).toBe(0);
       expect(game._componentsByType.test.length).toBe(1);
-    });
-  });
-
-  describe("addComponentToObject", function () {
-    it("should not do anything if the object does not exist", function () {
-      var result;
-      result = game.addComponentToObject('test', 'test', {});
-
-      expect(result).toBe(false);
-    });
-
-    it("should not do anything if the component does not exist", function () {
-      var result;
-      game.createObject('test');
-      result = game.addComponentToObject('test', 'test', {});
-
-      expect(result).toBe(false);
-    });
-
-    it("should add a new component instance to the object", function () {
-      game.addComponent('test', mock.component);
-      game.createObject('test');
-
-      result = game.addComponentToObject('test', 'test', {});
-
-      expect(result).toBe(true);
-
-      result = game.getObject('test');
-      expect(result.test).toBeDefined();
     });
   });
 });
