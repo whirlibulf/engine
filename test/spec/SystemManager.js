6fa56155087a0c51b91382b6807826aded3152d8
whirlibulf <whirlibulf.engine@gmail.com>
2013-07-08 13:53:13 +0100

    Rename system and component add function into use

--- test/spec/SystemManager.js
+++ test/spec/SystemManager.js
@@ -12,17 +12,17 @@ describe("SystemManager", function () {
         spyOn(mock, 'systemConstructor');
     });
 
-    describe("add", function () {
+    describe("use", function () {
         it("should return false if not passed an object", function () {
-            expect(systems.add()).toBe(false);
+            expect(systems.use()).toBe(false);
         });
 
         it("should return the system if added", function () {
-            expect(systems.add(mock.system)).toBe(mock.system);
+            expect(systems.use(mock.system)).toBe(mock.system);
         });
 
         it("should accept a constructor function instead of object", function () {
-            systems.add(mock.systemConstructor);
+            systems.use(mock.systemConstructor);
             expect(mock.systemConstructor).toHaveBeenCalled();
         });
     });
